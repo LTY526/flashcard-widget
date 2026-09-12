2f84f898bc53b738fecb658643363bacd43ba4b3
LTY526 <zzlolzzlol88@gmail.com>
2026-09-12 21:06:35 +0800

    Initial Commit

---
+++ flashcard-widget/flashcard_widgetApp.swift
@@ -0,0 +1,32 @@
+//
+//  flashcard_widgetApp.swift
+//  flashcard-widget
+//
+//  Created by User on 9/12/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct flashcard_widgetApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
